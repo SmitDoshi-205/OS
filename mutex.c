c99e5594989ac2f33560ae148a19805bf2e8cd90
Smit Doshi <smitdoshi205@gmail.com>
2025-05-08 07:59:59 +0530

    producer-consumer done

--- mutex.c
+++ mutex.c
@@ -1,62 +1,73 @@
-// Producer Consumer
 #include <stdio.h>
+
+#define SIZE 3  // Size of the buffer
 
 int mutex = 1;
 int full = 0;
-int empty = 3;
-int x = 0;
+int empty = SIZE;
+int buffer[SIZE];
+int in = 0;   // Index for producer
+int out = 0;  // Index for consumer
+int item = 0; // Item number to produce
 
-void produce(){
+void produce() {
     --mutex;
     ++full;
     --empty;
-    x++;
-    printf("Producer produced %d\n", x);
+
+    item++;
+    buffer[in] = item;
+    printf("Producer produced item %d at index %d\n", buffer[in], in);
+    in = (in + 1) % SIZE;
+
     ++mutex;
 }
 
-void consume(){
+void consume() {
     --mutex;
     --full;
     ++empty;
-    printf("Consumer consumed %d\n", x);
-    x--;
+
+    printf("Consumer consumed item %d from index %d\n", buffer[out], out);
+    buffer[out] = 0; // Optional: Clear the consumed spot
+    out = (out + 1) % SIZE;
+
     ++mutex;
 }
 
 int main() {
-    
     int choice = -1;
-    
+
     printf("1) Produce\n2) Consume\n3) Exit\n\n");
-    while (choice != 3){
+
+    while (choice != 3) {
         printf("Enter Choice: ");
         scanf("%d", &choice);
-        
-        switch(choice){
-            case 1: 
+
+        switch(choice) {
+            case 1:
                 if(mutex == 1 && empty != 0)
                     produce();
                 else
-                    printf("Wait\n");
+                    printf("Buffer full or busy. Wait.\n");
                 break;
-                
-            case 2: 
+
+            case 2:
                 if(mutex == 1 && full != 0)
                     consume();
                 else
-                    printf("Wait\n");
+                    printf("Buffer empty or busy. Wait.\n");
                 break;
-            case 3: 
-                printf("Exitting...\n");
+
+            case 3:
+                printf("Exiting...\n");
                 break;
-            default: 
-                printf("Enter proper choice\n");
+
+            default:
+                printf("Enter proper choice.\n");
                 break;
         }
     }
-    
-    
 
     return 0;
 }
